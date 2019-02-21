a54e37ca699d1a29f70312aded9c3575d367f298
mbohekale <mbohekale@yahoo.com>
2019-02-21 15:58:54 +0100

    passing a word to the server

--- Task-0/Client.java
+++ Task-0/Client.java
@@ -3,21 +3,23 @@ import java.net.*;
 import java.util.Scanner;
 public class Client{
  public static void main(String[] args)throws IOException{
-	String text="";
-		 
-    Scanner sc = new Scanner(System.in);		 
+	//1.Declaring the variable
+	String text="";		 
+   	Scanner sc = new Scanner(System.in);		 
 	
-	Socket s = new Socket("127.0.0.1",12345);
-	Scanner sc1 = new Scanner(s.getInputStream());
-	
+	 //2.Connecting to the server
+	 Socket s = new Socket("127.0.0.1",12345);
+	Scanner sc1 = new Scanner(s.getInputStream());	
 	PrintWriter pw = new PrintWriter(s.getOutputStream());
 	
-	System.out.println("Enter a Sentense:");
+	 //3.Passing the word to the server
+	 System.out.println("Enter a Sentense:");
 	text = sc.nextLine();
-	// from the server
+	//4.printing the message from the server
 	pw.println(text);
 	pw.flush();
 	
+	 //5.printing to the console
 	String temp = sc1.nextLine();
 	System.out.println(""+temp);
 	
