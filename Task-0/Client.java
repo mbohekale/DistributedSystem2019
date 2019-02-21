86d311f3927a9d4b611d06854c3a101aa8bcfb75
mbohekale <mbohekale@yahoo.com>
2019-02-21 16:25:36 +0100

    passing a number and calculating it

--- Task-0/Client.java
+++ Task-0/Client.java
@@ -1,30 +1,35 @@
-import java.io.*;
 import java.net.*;
-import java.util.Scanner;
-public class Client{
- public static void main(String[] args)throws IOException{
-	//1.Declaring the variable
-	String text="";		 
-   	Scanner sc = new Scanner(System.in);		 
-	
-	 //2.Connecting to the server
-	 Socket s = new Socket("127.0.0.1",12345);
-	Scanner sc1 = new Scanner(s.getInputStream());	
-	PrintWriter pw = new PrintWriter(s.getOutputStream());
-	
-	 //3.Passing the word to the server
-	 System.out.println("Enter a Sentense:");
-	text = sc.nextLine();
-	//4.printing the message from the server
-	pw.println(text);
-	pw.flush();
-	
-	 //5.printing to the console
-	String temp = sc1.nextLine();
-	System.out.println(""+temp);
-	
-	
- 
- }
-
+import java.io.*;
+import java.util.*;
+public class Client
+{
+	public static void main(String[] args)throws Exception{
+		try{
+		int number;
+		Scanner sc1 = new Scanner(System.in);
+		
+		
+				
+		int port=12345;
+		String computer = "localhost";
+		Socket s = new Socket(computer,port);
+		Scanner sc = new Scanner(s.getInputStream());
+		PrintWriter pw = new PrintWriter(s.getOutputStream());
+		
+		//sending it to the server
+		System.out.println("Please enter a number:");
+		number=sc1.nextInt();
+		
+		//printing it from the server
+		pw.println(number);
+		pw.flush();
+		
+		//printing on the console
+		int temp=sc.nextInt();
+		System.out.println(temp);
+		}catch(IOException e){
+				
+		System.out.println(e);
+		}
+	}
 }
